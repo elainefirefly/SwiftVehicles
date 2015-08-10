53b3891dbb7625d9c626acaa59e82828bef144fb
elainefirefly <elainefirefly@gmail.com>
2015-08-10 10:30:31 -0700

    Finished Part 1 of Vehicle

--- Vehicles/Truck.swift
+++ Vehicles/Truck.swift
@@ -11,6 +11,22 @@ import Foundation
 class Truck: Vehicle {
     var cargoCapacityCubicFeet: Int = 0
     
+    override var vehicleDetails: String {
+        // Get basic details from superclass
+        let basicDetails = super.vehicleDetails
+        
+        // Initialize mutable string
+        var truckDetailaBuilder = "\n\nTruck-Specific Details:\n\n"
+        
+        // Add info about truck-specific features
+        truckDetailaBuilder += "Cargo Capacity: \(cargoCapacityCubicFeet) cubic feet"
+        
+        //Create the final string by combining basic and truck-specific details
+        let truckDetails = basicDetails + truckDetailaBuilder
+        
+        return truckDetails
+    }
+    
     // Mark: - Supercalass overrides
     override func goForward() -> String {
         return String(format: "%@ Depress gas pedal.", changeGears("Drive"))
@@ -18,6 +34,26 @@ class Truck: Vehicle {
     
     override func stopMoving() -> String {
         return String(format: "Depress brake pedal, %@", changeGears("Park"))
+    }
+    
+    override func goBackward() -> String {
+        if cargoCapacityCubicFeet > 100 {
+            //Sound backup alarm first
+            return String(format: "Wait for \"%@\", then %@", soundBackupAlarm(), changeGears("Reverse"))
+        } else {
+            return String(format: "%@ Depress gas pedal.", changeGears("Reverse"))
+        }
+    }
+    
+    override func makeNoise() -> String {
+        switch numberOfWheels {
+        case Int.min...4:
+            return "Beep beep!"
+        case 5...8:
+            return "Honk!"
+        default:
+            return "HOOOOOOOOOOONK!"
+        }
     }
     
     // Mark: - Private methods
